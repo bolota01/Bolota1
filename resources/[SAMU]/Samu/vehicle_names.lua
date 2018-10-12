a7400a4ecbf42e99b430446e6248c4bd04c7fa7b
Guizao01 <37387447+Guizao01@users.noreply.github.com>
2018-10-12 18:26:41 -0300

    retirada fardas samu
    retirada fardas samu

--- resources/[SAMU]/Samu/vehicle_names.lua
+++
@@ -1,8 +0,0 @@
-function AddTextEntry(key, value)
-	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
-end
-
-Citizen.CreateThread(function()
-  AddTextEntry('0xC593CAF5', 'Samu')
-  AddTextEntry('0xC990C46A', 'Samu')
-end)
