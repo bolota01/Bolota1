4e051d2b46987f101dfda65ad12c23e6947d808d
Jubileu <linkinaron@hotmail.com>
2018-10-12 23:35:04 -0300

    Add remoção de dinheiro na compra de itens no lscustoms

--- resources/vrp_lscustoms/lscustoms_server.lua
+++ resources/vrp_lscustoms/lscustoms_server.lua
@@ -54,85 +54,21 @@ end)
 
 RegisterServerEvent("LSC:buttonSelected")
 AddEventHandler("LSC:buttonSelected", function(name, button)
-	local mymoney = 999999 --Just so you can buy everything while there is no money system implemented
-	if button.price then -- check if button have price
-		if button.price <= mymoney then
-			TriggerClientEvent("LSC:buttonSelected", source,name, button, true)
-			mymoney  = mymoney - button.price
-		else
-			TriggerClientEvent("LSC:buttonSelected", source,name, button, false)
-		end
+	local user_id = vRP.getUserId({source})
+
+	if button.price then
+		TriggerClientEvent(
+			"LSC:buttonSelected",
+			source,
+			name,
+			button,
+			vRP.tryFullPayment({user_id, button.price}))
 	end
 end)
 
 RegisterServerEvent("LSC:finished")
 AddEventHandler("LSC:finished", function(veh, playerId)
-	local model = veh.model --Display name from vehicle model(comet2, entityxf)
-	local mods = veh.mods
-	--[[
-	mods[0].mod - spoiler
-	mods[1].mod - front bumper
-	mods[2].mod - rearbumper
-	mods[3].mod - skirts
-	mods[4].mod - exhaust
-	mods[5].mod - roll cage
-	mods[6].mod - grille
-	mods[7].mod - hood
-	mods[8].mod - fenders
-	mods[10].mod - roof
-	mods[11].mod - engine
-	mods[12].mod - brakes
-	mods[13].mod - transmission
-	mods[14].mod - horn
-	mods[15].mod - suspension
-	mods[16].mod - armor
-	mods[23].mod - tires
-	mods[23].variation - custom tires
-	mods[24].mod - tires(Just for bikes, 23:front wheel 24:back wheel)
-	mods[24].variation - custom tires(Just for bikes, 23:front wheel 24:back wheel)
-	mods[25].mod - plate holder
-	mods[26].mod - vanity plates
-	mods[27].mod - trim design
-	mods[28].mod - ornaments
-	mods[29].mod - dashboard
-	mods[30].mod - dial design
-	mods[31].mod - doors
-	mods[32].mod - seats
-	mods[33].mod - steering wheels
-	mods[34].mod - shift leavers
-	mods[35].mod - plaques
-	mods[36].mod - speakers
-	mods[37].mod - trunk
-	mods[38].mod - hydraulics
-	mods[39].mod - engine block
-	mods[40].mod - cam cover
-	mods[41].mod - strut brace
-	mods[42].mod - arch cover
-	mods[43].mod - aerials
-	mods[44].mod - roof scoops
-	mods[45].mod - tank
-	mods[46].mod - doors
-	mods[48].mod - liveries
-	
-	--Toggle mods
-	mods[20].mod - tyre smoke
-	mods[22].mod - headlights
-	mods[18].mod - turbo
-	
-	--]]
-	local color = veh.color
-	local extracolor = veh.extracolor
-	local neoncolor = veh.neoncolor
-	local smokecolor = veh.smokecolor
-	local plateindex = veh.plateindex
-	local windowtint = veh.windowtint
-	local wheeltype = veh.wheeltype
-	local bulletProofTyres = veh.bulletProofTyres
-
 	local user_id = vRP.getUserId({source})
-
-	print(veh.model)
-	print(user_id)
 
 	MySQL.query(
 		"vRP/ls_sveh",
