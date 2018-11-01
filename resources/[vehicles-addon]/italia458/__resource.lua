f49a9955bd99220c584afc414ecf8e3868484b07
Guizao01 <37387447+Guizao01@users.noreply.github.com>
2018-11-01 14:38:13 -0300

    resource antigo italia458, novo ocorria erro de loading

--- resources/[vehicles-addon]/italia458/__resource.lua
+++ resources/[vehicles-addon]/italia458/__resource.lua
@@ -14,4 +14,7 @@ data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'   -- Not Required
 
+
+client_script {
+    'vehicle_names.lua'    -- Not Required
 }
