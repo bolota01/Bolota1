a27ca30026eff94c5cfcb834ad100628a3a79f59
Jubileu <linkinaron@hotmail.com>
2018-11-03 15:19:04 -0300

    Altera distancia da fala

--- resources/vrp/cfg/client.lua
+++ resources/vrp/cfg/client.lua
@@ -4,9 +4,9 @@ local cfg = {}
 
 cfg.iplload = true
 
-cfg.voice_proximity = 30.0 -- default voice proximity (outside)
-cfg.voice_proximity_vehicle = 5.0
-cfg.voice_proximity_inside = 9.0
+cfg.voice_proximity = 4 -- default voice proximity (outside)
+cfg.voice_proximity_vehicle = 3
+cfg.voice_proximity_inside = 3
 
 cfg.gui = {
   anchor_minimap_width = 186,
