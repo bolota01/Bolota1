f0184dc07b48c65a973994f479a9e40ac7941a72
Guizao01 <37387447+Guizao01@users.noreply.github.com>
2018-11-03 03:09:19 -0300

    add emotes com comandos

--- resources/emotes/cl.lua
+++ resources/emotes/cl.lua
@@ -1,108 +1,53 @@
 -- more emotes available at https://pastebin.com/6mrYTdQv
-local emotes = {    
-    ---------------Animções para Trabalhos-------------
-            ['pescar'] = "WORLD_HUMAN_STAND_FISHING",
-            --['Cavar'] = "world_human_gardener_plant",
-            ['exibida'] = "WORLD_HUMAN_PROSTITUTE_HIGH_CLASS",
-            ['se achando'] = "WORLD_HUMAN_PROSTITUTE_LOW_CLASS",
-            -------------ACADEMIA----------------
-            ['flexao'] = "WORLD_HUMAN_PUSH_UPS",
-            ['peso'] = "WORLD_HUMAN_MUSCLE_FREE_WEIGHTS",
-            ['fumar1'] = "WORLD_HUMAN_SMOKING",
-            ['policial'] = "WORLD_HUMAN_COP_IDLES",
-            ['encostar2'] = "WORLD_HUMAN_LEANING",
-            ['sentar1'] = "WORLD_HUMAN_PICNIC",
-            ['descancar'] = "WORLD_HUMAN_STUPOR",
-            ['deitar'] = "WORLD_HUMAN_SUNBATHE_BACK",
-            --['tomar sol'] = "WORLD_HUMAN_SUNBATHE",
-            ['curar'] = "CODE_HUMAN_MEDIC_TEND_TO_DEAD",
-            ['prancheta'] = "WORLD_HUMAN_CLIPBOARD",
-            ['beber2'] = "WORLD_HUMAN_PARTYING",
-            --['Observar'] = "CODE_HUMAN_MEDIC_KNEEL",
-            ['anotar'] = "CODE_HUMAN_MEDIC_TIME_OF_DEATH",
-            ['maconha1'] = "WORLD_HUMAN_SMOKING_POT",
-            ['impaciente'] = "WORLD_HUMAN_STAND_IMPATIENT",
-            ['soldar'] = "WORLD_HUMAN_WELDING",
-            ['foto'] = "WORLD_HUMAN_PAPARAZZI",
-            --['Filmadora'] = "WORLD_HUMAN_MOBILE_FILM_SHOCKING",
-            ['aplauso'] = "WORLD_HUMAN_CHEERING",
-            --['Binoculos'] = "WORLD_HUMAN_BINOCULARS",
-            ['musculos1'] = "WORLD_HUMAN_MUSCLE_FLEX",
-            ['yoga'] = "WORLD_HUMAN_YOGA",
-            ['ajuda'] = "WORLD_HUMAN_BUM_FREEWAY",
-            ['dormir'] = "WORLD_HUMAN_BUM_SLUMPED",
-            ['bobo'] = "WORLD_HUMAN_BUM_STANDING",
-            ['banho'] = "WORLD_HUMAN_BUM_WASH",
-            ['transito'] = "WORLD_HUMAN_CAR_PARK_ATTENDANT",
-            --['Palmas'] = "WORLD_HUMAN_CHEERING",
-            ['fumar2'] = "WORLD_HUMAN_AA_SMOKE",
-            ['britadeira'] = "WORLD_HUMAN_CONST_DRILL",
-            ['beber1'] = "WORLD_HUMAN_DRINKING",
-            ['maconha2'] = "WORLD_HUMAN_DRUG_DEALER",
-            --['Cheirar'] = "WORLD_HUMAN_DRUG_DEALER_HARD",
-            ['selfie'] = "WORLD_HUMAN_MOBILE_FILM_SHOCKING",
-            ['assoprador'] = "WORLD_HUMAN_GARDENER_LEAF_BLOWER",
-            --['golf'] = "WORLD_HUMAN_GOLF_PLAYER",
-            ['procurar'] = "WORLD_HUMAN_GUARD_PATROL",
-            ['guarda'] = "WORLD_HUMAN_GUARD_STAND",
-            ['martelo'] = "WORLD_HUMAN_HAMMERING",
-            --['Viu'] = "WORLD_HUMAN_HANG_OUT_STREET",
-            ['estatua'] = "WORLD_HUMAN_HUMAN_STATUE",
-            ['vassoura'] = "WORLD_HUMAN_JANITOR",
-            ['corrida'] = "WORLD_HUMAN_JOG_STANDING",
-            ['encostar1'] = "WORLD_HUMAN_LEANING",
-            ['limpar'] = "WORLD_HUMAN_MAID_CLEAN",
-            ['musculos2'] = "WORLD_HUMAN_MUSCLE_FLEX",
-            --['Muscle'] = "WORLD_HUMAN_MUSCLE_FREE_WEIGHTS",
-            ['musica'] = "WORLD_HUMAN_MUSICIAN",
-            --['Paparazzi'] = "WORLD_HUMAN_PAPARAZZI",
-            --['Cerveja'] = "WORLD_HUMAN_PARTYING",
-            --['Prostitue'] = "WORLD_HUMAN_PROSTITUTE_HIGH_CLASS",
-            --['Esquina'] = "WORLD_HUMAN_PROSTITUTE_LOW_CLASS",
-            ['sentar2'] = "WORLD_HUMAN_SEAT_LEDGE",
-            --['Subir'] = "WORLD_HUMAN_SEAT_STEPS",
-            ['sentar3'] = "WORLD_HUMAN_SEAT_WALL",
-            ['lanterna'] = "WORLD_HUMAN_SECURITY_SHINE_TORCH",
-            ['abdominal'] = "WORLD_HUMAN_SIT_UPS",
-            --['Fumadinha'] = "WORLD_HUMAN_SMOKING",
-            --['Ponta'] = "WORLD_HUMAN_SMOKING_POT",
-            ['esquentar'] = "WORLD_HUMAN_STAND_FIRE",
-            --['Fishing'] = "WORLD_HUMAN_STAND_FISHING",
-            ['pensando'] = "WORLD_HUMAN_STAND_IMPATIENT_UPRIGHT",
-            ['celular1'] = "WORLD_HUMAN_STAND_MOBILE",
-            ['celular2'] = "WORLD_HUMAN_STAND_MOBILE_UPRIGHT",
-            ['dancar1'] = "WORLD_HUMAN_STRIP_WATCH_STAND",
-            ['sentar4'] = "WORLD_HUMAN_STUPOR",
-            ['tomar sol'] = "WORLD_HUMAN_SUNBATHE",
-            --['Deitar'] = "WORLD_HUMAN_SUNBATHE_BACK",
-            ['tennis'] = "WORLD_HUMAN_TENNIS_PLAYER",
-            ['mapa'] = "WORLD_HUMAN_TOURIST_MAP",
-            ['celular3'] = "WORLD_HUMAN_TOURIST_MOBILE",
-            ['motor1'] = "WORLD_HUMAN_VEHICLE_MECHANIC",
-            ['window'] = "WORLD_HUMAN_WINDOW_SHOP_BROWSE",
-            --['Yoga'] = "WORLD_HUMAN_YOGA",
-            --['ATM'] = "PROP_HUMAN_ATM",
-            ['churrasco'] = "PROP_HUMAN_BBQ",
-            ['motor2'] = "PROP_HUMAN_BUM_BIN",
-            ['debrucar'] = "PROP_HUMAN_BUM_SHOPPING_CART",
-            ['barras'] = "PROP_HUMAN_MUSCLE_CHIN_UPS",
-            ['queisso'] = "PROP_HUMAN_PARKING_METER",
-            --['Bar'] = "PROP_HUMAN_SEAT_BAR",
-            ['sentar5'] = "PROP_HUMAN_SEAT_CHAIR",
-            ['sentar6'] = "PROP_HUMAN_SEAT_DECKCHAIR",
-            ['supino'] = "PROP_HUMAN_SEAT_MUSCLE_BENCH_PRESS_PRISON",
-            ['sentar7'] = "PROP_HUMAN_SEAT_CHAIR_MP_PLAYER",
-            ['supino2'] = "PROP_HUMAN_SEAT_MUSCLE_BENCH_PRESS",
-            ['banho sol'] = "PROP_HUMAN_SEAT_SUNLOUNGER",
-            ['olhando'] = "CODE_HUMAN_CROSS_ROAD_WAIT",
-            ['analizar'] = "CODE_HUMAN_MEDIC_KNEEL",
-            --['Multa'] = "CODE_HUMAN_MEDIC_TIME_OF_DEATH",
-            ['acalmar'] = "CODE_HUMAN_POLICE_CROWD_CONTROL",
-            ['investigar'] = "CODE_HUMAN_POLICE_INVESTIGATE"
-
-
-
-
+local emotes = {
+    ['fumar1'] = "WORLD_HUMAN_SMOKING",
+    ['fumar2'] = "WORLD_HUMAN_AA_SMOKE",
+    ['fumar3'] = "WORLD_HUMAN_DRUG_DEALER_HARD",
+    ['droga'] = "WORLD_HUMAN_SMOKING_POT",
+    ['droga2'] = "WORLD_HUMAN_DRUG_DEALER",
+    ['cop'] = "WORLD_HUMAN_COP_IDLES",
+    ['encostar'] = "WORLD_HUMAN_LEANING",
+    ['sentar1'] = "WORLD_HUMAN_PICNIC",
+    ['sentar2'] = "WORLD_HUMAN_STUPOR",
+    ['deitar'] = "WORLD_HUMAN_SUNBATHE_BACK",
+    ['tomarsol'] = "WORLD_HUMAN_SUNBATHE",
+    ['prancheta'] = "WORLD_HUMAN_CLIPBOARD",
+    ['beber1'] = "WORLD_HUMAN_DRINKING",
+    ['festa'] = "WORLD_HUMAN_PARTYING",
+    ['examinar'] = "CODE_HUMAN_MEDIC_KNEEL",
+    ['anotar'] = "CODE_HUMAN_MEDIC_TIME_OF_DEATH",
+    ['impaciente'] = "WORLD_HUMAN_STAND_IMPATIENT",
+    ['pescar'] = "WORLD_HUMAN_STAND_FISHING",
+    ['soldar'] = "WORLD_HUMAN_WELDING",
+    ['foto'] = "WORLD_HUMAN_PAPARAZZI",
+    ['filmar'] = "WORLD_HUMAN_MOBILE_FILM_SHOCKING",
+    ['palmas'] = "WORLD_HUMAN_CHEERING",
+    ['musculo'] = "WORLD_HUMAN_MUSCLE_FLEX",
+    ['pesos'] = "WORLD_HUMAN_MUSCLE_FREE_WEIGHTS",
+    ['flexao'] = "WORLD_HUMAN_PUSH_UPS",
+    ['abdominal'] = "WORLD_HUMAN_SIT_UPS",
+    ['barras1'] = "PROP_HUMAN_MUSCLE_CHIN_UPS",
+    ['barras2'] = "PROP_HUMAN_MUSCLE_CHIN_UPS_ARMY",
+    ['yoga'] = "WORLD_HUMAN_YOGA",
+    ['dormir'] = "WORLD_HUMAN_BUM_SLUMPED",
+    ['bobo'] = "WORLD_HUMAN_BUM_STANDING",
+    ['sinalizar'] = "WORLD_HUMAN_CAR_PARK_ATTENDANT",
+    ['britadeira'] = "WORLD_HUMAN_CONST_DRILL",
+    ['assoprador'] = "WORLD_HUMAN_GARDENER_LEAF_BLOWER",
+    ['martelo'] = "WORLD_HUMAN_HAMMERING",
+    ['estatua'] = "WORLD_HUMAN_HUMAN_STATUE",
+    ['limpar'] = "WORLD_HUMAN_MAID_CLEAN",
+    ['instrumento'] = "WORLD_HUMAN_MUSICIAN",
+    ['aquecer'] = "WORLD_HUMAN_STAND_FIRE",
+    ['dança1'] = "WORLD_HUMAN_STRIP_WATCH_STAND",
+    ['tenis'] = "WORLD_HUMAN_TENNIS_PLAYER",
+    ['mapa'] = "WORLD_HUMAN_TOURIST_MAP",
+    ['motor1'] = "WORLD_HUMAN_VEHICLE_MECHANIC",
+    ['motor2'] = "PROP_HUMAN_BUM_BIN",
+    ['churrasco'] = "PROP_HUMAN_BBQ",
+    ['cafe'] = "WORLD_HUMAN_AA_COFFEE",
+    ['implorar'] = "WORLD_HUMAN_BUM_FREEWAY",
+    ['celular'] = "WORLD_HUMAN_STAND_MOBILE"
 }
 
 --[[------------------------------------------------------------------------------------------------
@@ -137,7 +82,7 @@ end
 
 function displayEmotes()
     local index = 0 -- Current index
-    local display = "^2" -- Text to display
+    local display = "^7" -- Text to display
 
     for name, value in pairs(emotes) do -- Adding the emote names to the display var
         index = index + 1
@@ -148,7 +93,7 @@ function displayEmotes()
         end
     end
 
-    TriggerEvent("chatMessage", "ANIMACOES: /e <nome da animacao>", {255,0,0}, "") -- Saying "EMOTES:" in red
+    TriggerEvent("chatMessage", "EMOTES", {255,0,0}, "") -- Saying "EMOTES:" in red
     TriggerEvent("chatMessage", "", {0,0,0}, display) -- Displaying the emotes in white
 end
 function playEmote(emoteDic) -- Plays an emote from the given name dictionary
@@ -175,10 +120,10 @@ end
 AddEventHandler("emote:invoke", function(name)
     if emotes[name] ~= nil then -- Checking if the name is in the dictionary
         if playEmote(emotes[name]) then -- Playing the emote from the dictionary
-            drawNotification("Fazendo Animções \""..name.."\"")
+            drawNotification("Playing the emote \""..name.."\"")
         end
     else
-        TriggerEvent("chatMessage", "ERROR", {255,0,0}, "Nome de Animção inválido") -- Saying if the name wasn't in the dictionary
+        TriggerEvent("chatMessage", "ERROR", {255,0,0}, "Comando emote inválido") -- Saying if the name wasn't in the dictionary
     end
 end)
 AddEventHandler("emote:display", function()
