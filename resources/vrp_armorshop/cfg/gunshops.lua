c74135f680256fe6d2541ac99740ca142bfa3c21
Guizao01 <37387447+Guizao01@users.noreply.github.com>
2018-11-13 16:21:29 -0200

    alterada as armas vendidas em cada loja de armas

--- resources/vrp_armorshop/cfg/gunshops.lua
+++ resources/vrp_armorshop/cfg/gunshops.lua
@@ -9,7 +9,22 @@ local cfg = {}
 -- _config: blipid, blipcolor, permissions (optional, only users with the permission will have access to the shop)
 
 cfg.gunshop_types = {
-  ["melees"] = {
+  ["Loja de Armas 01"] = {
+    _config = {blipid=110,blipcolor=75},
+    --["WEAPON_BOTTLE"] = {"Garrafa Quebrada",600,0,""},
+    --["WEAPON_BAT"] = {"Taco de Beisebol",1500,0,""},
+    ["WEAPON_HAMMER"] = {"Martelo",1500,0,""},
+    --["WEAPON_GOLFCLUB"] = {"Taco de Golf",1500,0,""},
+    ["WEAPON_SWITCHBLADE"] = {"Canivete",1500,0,""},
+    ["WEAPON_WRENCH"] = {"Chave Inglesa",1000,0,""},
+    ["WEAPON_KNUCKLE"] = {"Soco-Inglês",1000,0,""},
+    ["WEAPON_KNIFE"] = {"Faca",1500,0,""},
+    ["WEAPON_PISTOL"] = {"Pistola",30000,150,""},
+    ["WEAPON_FLASHLIGHT"] = {"Lanterna",1000,0,""}
+    --["WEAPON_MICROSMG"] = {"Micro Uzi",60000,300,""}
+  },
+
+  ["Loja de Armas 02"] = {
     _config = {blipid=110,blipcolor=75},
     ["WEAPON_BOTTLE"] = {"Garrafa Quebrada",600,0,""},
     ["WEAPON_BAT"] = {"Taco de Beisebol",1500,0,""},
@@ -24,7 +39,7 @@ cfg.gunshop_types = {
     ["WEAPON_MICROSMG"] = {"Micro Uzi",60000,300,""}
   },
 
-  ["sidearms"] = {
+  --[[["submachineguns"] = {
     _config = {blipid=110,blipcolor=75},
     ["WEAPON_BOTTLE"] = {"Garrafa Quebrada",600,0,""},
     ["WEAPON_BAT"] = {"Taco de Beisebol",1500,0,""},
@@ -37,39 +52,24 @@ cfg.gunshop_types = {
     ["WEAPON_PISTOL"] = {"Pistola",30000,150,""},
     ["WEAPON_FLASHLIGHT"] = {"Lanterna",1000,0,""},
     ["WEAPON_MICROSMG"] = {"Micro Uzi",60000,300,""}
-  },
+  },- ]]
 
-  ["submachineguns"] = {
+  ["Loja de Armas 03"] = {
     _config = {blipid=110,blipcolor=75},
     ["WEAPON_BOTTLE"] = {"Garrafa Quebrada",600,0,""},
-    ["WEAPON_BAT"] = {"Taco de Beisebol",1500,0,""},
-    ["WEAPON_HAMMER"] = {"Martelo",1500,0,""},
-    ["WEAPON_GOLFCLUB"] = {"Taco de Golf",1500,0,""},
+    --["WEAPON_BAT"] = {"Taco de Beisebol",1500,0,""},
+    --["WEAPON_HAMMER"] = {"Martelo",1500,0,""},
+    --["WEAPON_GOLFCLUB"] = {"Taco de Golf",1500,0,""},
     ["WEAPON_SWITCHBLADE"] = {"Canivete",1500,0,""},
-    ["WEAPON_WRENCH"] = {"Chave Inglesa",1000,0,""},
+    --["WEAPON_WRENCH"] = {"Chave Inglesa",1000,0,""},
     ["WEAPON_KNUCKLE"] = {"Soco-Inglês",1000,0,""},
     ["WEAPON_KNIFE"] = {"Faca",1500,0,""},
-    ["WEAPON_PISTOL"] = {"Pistola",30000,150,""},
-    ["WEAPON_FLASHLIGHT"] = {"Lanterna",1000,0,""},
+    --["WEAPON_PISTOL"] = {"Pistola",30000,150,""},
+    --["WEAPON_FLASHLIGHT"] = {"Lanterna",1000,0,""},
     ["WEAPON_MICROSMG"] = {"Micro Uzi",60000,300,""}
   },
 
-  ["rifles"] = {
-    _config = {blipid=110,blipcolor=75},
-    ["WEAPON_BOTTLE"] = {"Garrafa Quebrada",600,0,""},
-    ["WEAPON_BAT"] = {"Taco de Beisebol",1500,0,""},
-    ["WEAPON_HAMMER"] = {"Martelo",1500,0,""},
-    ["WEAPON_GOLFCLUB"] = {"Taco de Golf",1500,0,""},
-    ["WEAPON_SWITCHBLADE"] = {"Canivete",1500,0,""},
-    ["WEAPON_WRENCH"] = {"Chave Inglesa",1000,0,""},
-    ["WEAPON_KNUCKLE"] = {"Soco-Inglês",1000,0,""},
-    ["WEAPON_KNIFE"] = {"Faca",1500,0,""},
-    ["WEAPON_PISTOL"] = {"Pistola",30000,150,""},
-    ["WEAPON_FLASHLIGHT"] = {"Lanterna",1000,0,""},
-    ["WEAPON_MICROSMG"] = {"Micro Uzi",60000,300,""}
-  },
-
-  ["heavymachineguns"] = {
+  --[[["heavymachineguns"] = {
     _config = {blipid=110,blipcolor=75},
     ["WEAPON_BOTTLE"] = {"Garrafa Quebrada",600,0,""},
     ["WEAPON_BAT"] = {"Taco de Beisebol",1500,0,""},
@@ -112,9 +112,9 @@ cfg.gunshop_types = {
     ["WEAPON_PISTOL"] = {"Pistola",30000,150,""},
     ["WEAPON_FLASHLIGHT"] = {"Lanterna",1000,0,""},
     ["WEAPON_MICROSMG"] = {"Micro Uzi",60000,300,""}
-  },
+  },- ]]
 
-  ["shotguns"] = {
+  ["Loja de Armas 04"] = {
     _config = {blipid=110,blipcolor=75},
     ["WEAPON_BOTTLE"] = {"Garrafa Quebrada",600,0,""},
     ["WEAPON_BAT"] = {"Taco de Beisebol",1500,0,""},
@@ -124,24 +124,24 @@ cfg.gunshop_types = {
     ["WEAPON_WRENCH"] = {"Chave Inglesa",1000,0,""},
     ["WEAPON_KNUCKLE"] = {"Soco-Inglês",1000,0,""},
     ["WEAPON_KNIFE"] = {"Faca",1500,0,""},
-    ["WEAPON_PISTOL"] = {"Pistola",30000,150,""},
-    ["WEAPON_FLASHLIGHT"] = {"Lanterna",1000,0,""},
-    ["WEAPON_MICROSMG"] = {"Micro Uzi",60000,300,""}
+    --["WEAPON_PISTOL"] = {"Pistola",30000,150,""},
+    ["WEAPON_FLASHLIGHT"] = {"Lanterna",1000,0,""}
+    --["WEAPON_MICROSMG"] = {"Micro Uzi",60000,300,""}
   },
 
-  ["explosives"] = {
+  ["Loja de Armas 05"] = {
     _config = {blipid=110,blipcolor=75},
-    ["WEAPON_BOTTLE"] = {"Garrafa Quebrada",600,0,""},
-    ["WEAPON_BAT"] = {"Taco de Beisebol",1500,0,""},
+    --["WEAPON_BOTTLE"] = {"Garrafa Quebrada",600,0,""},
+    --["WEAPON_BAT"] = {"Taco de Beisebol",1500,0,""},
     ["WEAPON_HAMMER"] = {"Martelo",1500,0,""},
-    ["WEAPON_GOLFCLUB"] = {"Taco de Golf",1500,0,""},
+    --["WEAPON_GOLFCLUB"] = {"Taco de Golf",1500,0,""},
     ["WEAPON_SWITCHBLADE"] = {"Canivete",1500,0,""},
-    ["WEAPON_WRENCH"] = {"Chave Inglesa",1000,0,""},
+    --["ARMOR"] = {"Colete a prova de balas",10000,0,""},
     ["WEAPON_KNUCKLE"] = {"Soco-Inglês",1000,0,""},
     ["WEAPON_KNIFE"] = {"Faca",1500,0,""},
     ["WEAPON_PISTOL"] = {"Pistola",30000,150,""},
-    ["WEAPON_FLASHLIGHT"] = {"Lanterna",1000,0,""},
-    ["WEAPON_MICROSMG"] = {"Micro Uzi",60000,300,""}
+    ["WEAPON_FLASHLIGHT"] = {"Lanterna",1000,0,""}
+    --["WEAPON_MICROSMG"] = {"Micro Uzi",60000,300,""}
   },
 
   ["Loja de Paraquedas"] = {
@@ -149,37 +149,37 @@ cfg.gunshop_types = {
     ["GADGET_PARACHUTE"] = {"Paraquedas",2000,0,""}
   },
 
-  ["basic"] = {
+  ["Loja de Armas 06"] = {
     _config = {blipid=110,blipcolor=75},
-    ["WEAPON_BOTTLE"] = {"Garrafa Quebrada",600,0,""},
-    ["WEAPON_BAT"] = {"Taco de Beisebol",1500,0,""},
+    --["WEAPON_BOTTLE"] = {"Garrafa Quebrada",600,0,""},
+    --["WEAPON_BAT"] = {"Taco de Beisebol",1500,0,""},
     ["WEAPON_HAMMER"] = {"Martelo",1500,0,""},
-    ["WEAPON_GOLFCLUB"] = {"Taco de Golf",1500,0,""},
+    --["WEAPON_GOLFCLUB"] = {"Taco de Golf",1500,0,""},
     ["WEAPON_SWITCHBLADE"] = {"Canivete",1500,0,""},
     ["WEAPON_WRENCH"] = {"Chave Inglesa",1000,0,""},
     ["WEAPON_KNUCKLE"] = {"Soco-Inglês",1000,0,""},
     ["WEAPON_KNIFE"] = {"Faca",1500,0,""},
     ["WEAPON_PISTOL"] = {"Pistola",30000,150,""},
-    ["WEAPON_FLASHLIGHT"] = {"Lanterna",1000,0,""},
-    ["WEAPON_MICROSMG"] = {"Micro Uzi",60000,300,""}
+    ["WEAPON_FLASHLIGHT"] = {"Lanterna",1000,0,""}
+    --["WEAPON_MICROSMG"] = {"Micro Uzi",60000,300,""}
   }
 }
 
 -- list of gunshops positions
 
 cfg.gunshops = {
-  {"melees", 1692.41, 3758.22, 34.7053},
-  {"sidearms", 252.696, -48.2487, 69.941},
-  {"carabines", 844.299, -1033.26, 28.1949},
+  {"Loja de Armas 01", 1693.6977539063,3759.8000488281,34.70531463623},
+  {"Loja de Armas 02", 251.91516113281,-49.788932800293,69.941070556641},
+  --{"carabines", 844.299, -1033.26, 28.1949},
   {"Loja de Paraquedas", 501.35134887695,5595.5263671875,795.90059179688},
-  {"rifles", -331.624, 6082.46, 31.4548},
-  {"submachineguns", -664.147, -935.119, 21.8292},
-  {"sidearms", -1305.45056152344,-394.0068359375,36.6957740783691},
-  {"explosives", -1119.48803710938,2697.08666992188,18.5541591644287},
-  {"heavymachineguns", 2569.62, 294.453, 108.735},
-  {"snipers", -3172.60375976563,1085.74816894531,20.8387603759766},
-  {"shotguns", 21.70, -1107.41, 29.79},
-  {"basic", 810.15, -2156.88, 29.61}
+  {"Loja de Armas 03", -330.2470703125,6083.9926757813,31.454767227173},
+  --{"submachineguns", -664.147, -935.119, 21.8292},
+  --{"sidearms", -1305.45056152344,-394.0068359375,36.6957740783691},
+  {"Loja de Armas 05", -1117.6557617188,2698.6552734375,18.554130554199},
+  --{"heavymachineguns", 2569.62, 294.453, 108.735},
+  --{"snipers", -3172.60375976563,1085.74816894531,20.8387603759766},
+  {"Loja de Armas 04", 21.70, -1107.41, 29.90},
+  {"Loja de Armas 06", 810.15, -2156.88, 29.89}
 }
 
 return cfg
