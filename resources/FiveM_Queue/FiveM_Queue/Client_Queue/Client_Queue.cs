46f3c51139cc9c8bb021fdbd3db0df7ffa0b168b
Jubileu <linkinaron@hotmail.com>
2018-11-14 00:09:46 -0200

    Add Fivem queue no servidor

---
+++ resources/FiveM_Queue/FiveM_Queue/Client_Queue/Client_Queue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+
+namespace Client_Queue
+{
+    public class Client_Queue : BaseScript
+    {
+        public Client_Queue()
+        {
+            Tick += Connected;
+        }
+
+        private async Task Connected()
+        {
+            try
+            {
+                while (!API.NetworkIsPlayerActive(API.PlayerId()))
+                {
+                    await Delay(1000);
+                }
+                TriggerServerEvent("fivemqueue: playerConnected");
+                API.SendNuiMessage($@"{{ ""resname"" : ""{API.GetCurrentResourceName()}"" }}");
+                Tick -= Connected;
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine($"[{API.GetCurrentResourceName()} - Client_Queue ERROR] - Connected()");
+            }
+        }
+    }
+}
