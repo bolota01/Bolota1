01fb6ab5ac76f3e9a8954348a49670b67a31a3d8
Guizao01 <37387447+Guizao01@users.noreply.github.com>
2018-11-15 20:31:06 -0200

    van e helicoptero do jornal

---
+++ resources/[vehicles-replace]/eliten/__resource.lua
@@ -0,0 +1,11 @@
+
+
+files {
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
