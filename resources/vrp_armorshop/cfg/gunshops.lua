b99be8c21e43773d8b5c31be2a73fe6ed31c0e3a
Guizao01 <37387447+Guizao01@users.noreply.github.com>
2018-11-30 18:17:57 -0200

    alterada a forma de pegar armas na loja de armas da policia

--- resources/vrp_armorshop/cfg/gunshops.lua
+++ resources/vrp_armorshop/cfg/gunshops.lua
@@ -40,14 +40,14 @@ cfg.gunshop_types = {
   },
 
   ["Loja da Polícia"] = {
-    _config = {blipid=110,blipcolor=75,permissions={"policiafederal.weapons"}},
-    --["WEAPON_BOTTLE"] = {"Garrafa Quebrada",600,0,""},
-    --["WEAPON_BAT"] = {"Taco de Beisebol",1500,0,""},
-    --["WEAPON_HAMMER"] = {"Martelo",1500,0,""},
-    --["WEAPON_GOLFCLUB"] = {"Taco de Golf",1500,0,""},
-    --["WEAPON_SWITCHBLADE"] = {"Canivete",1500,0,""},
-    --["WEAPON_WRENCH"] = {"Chave Inglesa",1000,0,""},
-    --["WEAPON_KNUCKLE"] = {"Soco-Inglês",1000,0,""},
+    _config = {blipid=110,blipcolor=75,permissions={"controle.armorshop"}},
+    ["WEAPON_COMBATPISTOL"] = {"Pistola de Combate",1,1,""},
+    ["WEAPON_NIGHTSTICK"] = {"Cacetete",1,0,""},
+    ["WEAPON_STUNGUN"] = {"Arma de Choque",1,0,""},
+    ["WEAPON_SMG"] = {"Submetralhadora",1,1,""},
+    ["WEAPON_CARBINERIFLE"] = {"Carabina",1,1,""},
+    ["WEAPON_SPECIALCARBINE"] = {"Carabina Especial",1,1,""},
+    ["WEAPON_SNIPERRIFLE"] = {"Sniper",1,1,""},
     --["WEAPON_KNIFE"] = {"Faca",1500,0,""},
     --["WEAPON_PISTOL"] = {"Pistola",30000,150,""},
     ["ARMOR"] = {"Colete",0,0,""}
@@ -189,7 +189,7 @@ cfg.gunshop_types = {
 cfg.gunshops = {
   {"Loja de Armas 01", 1693.6977539063,3759.8000488281,34.70531463623},
   {"Loja de Armas 02", 251.91516113281,-49.788932800293,69.941070556641},
-  {"Loja da Polícia", 459.84631347656,-979.43682861328,30.68959236145},
+  {"Loja da Polícia", 461.46166992188,-981.07891845703,30.689590454102},
   {"Loja Admin", -77.116828918457,-825.8935546875,284.99993896484},
   --{"carabines", 844.299, -1033.26, 28.1949},
   {"Loja de Paraquedas", 501.35134887695,5595.5263671875,795.90059179688},
